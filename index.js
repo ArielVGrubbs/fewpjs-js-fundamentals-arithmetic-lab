4bb3c17421c3c50106009613ae36d5767bc4ef51
ariel_grubbs <arielvictor1234@gmail.com>
2020-11-16 19:18:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+let random = Math.floor( Math.random() * 1000 ) + 1
+let mod = 64 % 10
+let max = Math.max(20, 1, 2, 3, 4)
